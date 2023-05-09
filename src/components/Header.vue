e6161f5a81c13870c6a8bd2873ac2a5abf22d74a
maricamola <maricamola98@libero.it>
2023-05-09 15:58:17 +0200

    header added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,8 +7,94 @@ export default {
 
 <template>
   <header>
-    <h1>Header</h1>
+    <div class="container-fluid d-flex justify-content-between align-items-center">
+      <img src="logo-gobike.png" alt="logo">
+
+      <div class="navbar d-flex">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Training</li>
+          <li>Packages</li>
+          <li>Blog</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+
+      <div class="backdrop d-flex align-items-center">
+        <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop"
+          aria-controls="staticBackdrop">
+          <i class="fa-solid fa-bars"></i>
+
+        </button>
+
+        <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop"
+          aria-labelledby="staticBackdropLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="staticBackdropLabel">Offcanvas</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <div>
+              I will not close if you click outside of me.
+            </div>
+          </div>
+        </div>
+
+        <!-- Bottone con immagine -->
+        <div class="toggle">
+          <span>
+            <h6>Upcoming Events <i class="fa-solid fa-arrow-right"></i></h6>
+          </span>
+        </div>
+      </div>
+
+    </div>
   </header>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+@use '../scss/main.scss';
+@use '../scss/partials/vars.scss';
+
+header {
+  background-color: aquamarine;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+
+  .container-fluid {
+    position: fixed;
+    align-items: center;
+    padding: 10px 40px;
+  }
+
+  img {
+    width: 15%
+  }
+
+  .navbar ul {
+    list-style: none;
+    display: flex;
+  }
+
+  ul li {
+    margin: 0 10px;
+    font-size: 18px;
+  }
+
+  .backdrop i {
+    font-size: 40px;
+  }
+
+  .toggle {
+    border: 1px solid black;
+    padding: 20px;
+  }
+
+  .toggle i {
+    font-size: 10px;
+  }
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,7 +6,12 @@ export default {
 
 <template>
   <main>
-    <h1>Main</h1>
+    <section>Foto grande con timing</section>
+    <section>Foto in movimento</section>
+    <section>Cards slider</section>
+    <section>Cards events stile bootstrap</section>
+    <section>Cards profili trainers</section>
+    <section>Cards pacchetti</section>
   </main>
 </template>
 
