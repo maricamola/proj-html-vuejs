678c7f8431d2c831befa84ca52d53e6dae4ebbb5
maricamola <maricamola98@libero.it>
2023-05-10 17:14:04 +0200

    offcanvas completed

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -28,16 +28,32 @@ export default {
           <i class="fa-solid fa-bars"></i>
 
         </button>
-
         <div class="offcanvas offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop"
-          aria-labelledby="staticBackdropLabel">
-          <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="staticBackdropLabel">Offcanvas</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-          </div>
+          aria-labelledby="staticBackdropLabel" data-bs-dismiss="offcanvas">
           <div class="offcanvas-body">
             <div>
-              I will not close if you click outside of me.
+              <img class="loader" src="loader-gobike.png" alt="">
+            </div>
+            <div class="contact">
+              <h2>Contact Information</h2>
+              <span><i class="fa-solid fa-phone"></i></span>
+              <p>
+                +1 (234) 567 89 10
+              </p>
+              <span><i class="fa-regular fa-envelope"></i></span>
+              <p>
+                example@example.com
+              </p>
+              <span><i class="fa-solid fa-location-dot"></i></span>
+              <p>
+                New Jersey, USA
+              </p>
+              <span class="social">
+                <i class="fa-brands fa-facebook-f"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-twitter"></i>
+              </span>
+              <span class="copyright">© 2023 - Gobike</span>
             </div>
           </div>
         </div>
@@ -98,5 +114,60 @@ header {
   .toggle i {
     font-size: 10px;
   }
+}
+
+// OFFCANVAS
+.offcanvas {
+  text-align: center;
+  display: flex;
+
+  h2 {
+    margin: 10px;
+  }
+
+  p {
+    font-size: 20px;
+  }
+
+  .copyright {
+    display: block;
+    margin-top: 10px;
+  }
+}
+
+.offcanvas i {
+  margin: 10px;
+  font-size: 60px;
+  color: white;
+  background-color: black;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.offcanvas-body {
+  margin: 20px;
+
+  .social i {
+    color: black;
+    background-color: white;
+    padding: 10px 5px;
+  }
+}
+
+.loader {
+  width: 50%;
+  margin-bottom: 10px;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  background-color: #f1f1f1;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #000000;
 }
 </style>
